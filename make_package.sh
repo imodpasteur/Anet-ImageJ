0403f151daa757e7a59abfb4da11e0e5af5af9df
oeway <oeway007@gmail.com>
2018-05-24 19:21:20 +0200

    improve gif and readme

--- make_package.sh
+++ make_package.sh
@@ -1,2 +1,2 @@
 mvn clean compile assembly:single
-cp ./target/ANNA_PALM_Process-0.2.0-SNAPSHOT-jar-with-dependencies.jar /Applications/Fiji.app/plugins/
+echo "the plugin file is located in the folder named 'target'"
